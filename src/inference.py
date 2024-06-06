5b6962cac03496785a71fa1f86f9e3433c7657d6
yaizasear <104634047+yaizasear@users.noreply.github.com>
2024-06-06 17:35:14 +0200

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -12,39 +12,29 @@ from torch import set_float32_matmul_precision
 
 
 def beam_search(model, input_sequence, vocab, beam_width, device='cuda'):
-    # Create a list to store the top-k candidates at each step.
     candidates = [[input_sequence, 0.0]]
-    # Initialize the output text list.
     output_text = []
     input_sequence = torch.LongTensor([input_sequence]).to(device)
     encoded_x = model.encode(input_sequence) # (Sx, B, E)
-    # Repeat the generation process until the maximum length is reached or no candidate is left.
+    
     for i in range(512): # 330 sequence length
-        # Initialize the list to store the new candidates.
         new_candidates = []
-        # Iterate over the current top-k candidates.
         for candidate, score in candidates:
-            # Predict the next word (aa) using the current candidate as input.
             input_sequence = torch.LongTensor([candidate]).to(device)
             output = model.decode(input_sequence, encoded_x)
             logits = torch.nn.functional.softmax(output, dim=-1).detach().cpu().numpy()[0, -1, :]
-            # Compute the log probabilities of each word in the vocabulary.
             log_probs = np.log(np.exp(logits) / np.sum(np.exp(logits)))
-            # Get the indices of the top-k words.
             top_k_indices = np.argsort(log_probs)[-beam_width:]
-            # Add the new candidates to the list.
             for index in top_k_indices:
                 new_candidate = candidate + [index]
                 new_score = score + log_probs[index]
                 new_candidates.append([new_candidate, new_score])
-        # Sort the new candidates by their score.
+                
         new_candidates = sorted(new_candidates, key=lambda x: x[1], reverse=True)
-        # Keep the top-k candidates.
         candidates = new_candidates[:beam_width]
-        # Stop if no candidate is left.
         if not candidates:
             break
-    # Convert the final candidate to text.
+
     for candidate, _ in candidates:
         sequence = ""
         for index in candidate:
@@ -81,7 +71,7 @@ def main(args, config):
     fabric.load(weights_path, state)
 
     # Generate sequences
-    num_sequences = 1 #10000 // args.k
+    num_sequences = 1
     input_sequence = [0] # START  token
     output_sequences = []
     fabric.print("Generating from", args.weights_path, "...\n")
